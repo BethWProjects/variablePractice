00375f7cd14694da6f1b7f01875b9cf3c1f75bd0
Beth Wilson <bethwilson777@gmail.com>
2022-06-07 15:20:59 -0600

    Updated console.log statement

--- variable.js
+++ variable.js
@@ -6,8 +6,6 @@ var entryAge = 18
 var entryAgeMet = true
 var dogsAllowed = false
 var name = "Lynn"
-console.log(name)
 var age = 32
-console.log(age)
 var dogsAllowed = true
 console.log(dogsAllowed)
