a5bb0bbd5bf1eaddc237c97e508a3fd683eede05
Beth Wilson <bethwilson777@gmail.com>
2022-06-07 15:17:01 -0600

    Add console.log statements

--- variable.js
+++ variable.js
@@ -5,3 +5,9 @@ var age = 42
 var entryAge = 18
 var entryAgeMet = true
 var dogsAllowed = false
+var name = "Lynn"
+console.log(name)
+var age = 32
+console.log(age)
+var dogsAllowed = true
+console.log(dogsAllowed)
